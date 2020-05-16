5a36346ac864b066993959a896fa1a69a5f54d7a
Sam Pratt <sampratt@SamPratt-MacBook-Pro.local>
2020-05-16 15:56:14 +0100

    Added Sensor sketches

---
+++ MultiSensorBoard/APDS-9960/APDS-9960.ino
@@ -0,0 +1,28 @@
+#include <Arduino_APDS9960.h>
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  if (!APDS.begin()) {
+    //This seems to always happen on my board!
+    Serial.println("Error initializing APDS9960 sensor!");
+  }
+}
+
+void loop() {
+  // check if a proximity reading is available
+  if (APDS.proximityAvailable()) {
+    // read the proximity
+    // - 0   => close
+    // - 255 => far
+    // - -1  => error
+    int proximity = APDS.readProximity();
+
+    // print value to the Serial Monitor
+    Serial.println(proximity);
+  }
+
+  // wait a bit before reading again
+  delay(100);
+}
